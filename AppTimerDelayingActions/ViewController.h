937b2b8f231c68fea1b700acb060cf2390021554
MiriamASanchezC <miriamaliciasanchezcervantes@gmail.com>
2025-02-19 21:21:53 -0600

    App timer

--- AppTimerDelayingActions/ViewController.h
+++ AppTimerDelayingActions/ViewController.h
@@ -8,6 +8,10 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *etiqueta;
+
+- (IBAction)delayAction:(id)sender;
+
 
 
 @end
--- AppTimerDelayingActions/ViewController.m
+++ AppTimerDelayingActions/ViewController.m
@@ -19,4 +19,16 @@
 }
 
 
+- (IBAction)delayAction:(id)sender {
+    [self performSelector:@selector(delay1) withObject:nil afterDelay:5.0];
+    [self performSelector:@selector(delay2) withObject:nil afterDelay:9.0];
+}
+
+-(void)delay1{
+    self.etiqueta.text = @"El restraso esta empezando";
+}
+
+-(void)delay2{
+    self.etiqueta.text= @"Byeee...";
+}
 @end
